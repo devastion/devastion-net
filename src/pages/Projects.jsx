3919e2e6fe6af607be719ba1a9b727d04d068296
Dimitar Banev <banev_dimitar@mail.com>
2022-03-23 05:21:51 +0200

    Update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,28 +5,73 @@ import folder from "../images/folder-v2.png";
 function Projects() {
   const url = "https://api.github.com/users/devastion/repos";
   const [repos, setRepos] = useState([]);
-  const reposFetch = async () => {
-    const res = await fetch(url);
+  const [langs, setLangs] = useState([]);
+
+  const reposFetch = async (link, setArray) => {
+    const res = await fetch(link);
     const data = await res.json();
-    setRepos(data);
+    setArray(data);
   };
 
   useEffect(async () => {
-    reposFetch();
+    reposFetch(url, setRepos);
   }, []);
 
+  function addClass(lang) {
+    if (lang === "JavaScript") {
+      return "red";
+    }
+
+    if (lang === "PHP") {
+      return "purple";
+    }
+
+    if (lang === "HTML") {
+      return "green";
+    }
+
+    if (lang === "C++") {
+      return "blue";
+    }
+
+    return "grey";
+  }
+
   const listedRepos = repos.map((repo) => (
-    <div className="repo__card" key={repo.fullName}>
-      <h1 key={repo.name}>
-        <img src={folder} alt="folder" width="38px" height="36px" />
-        {repo.name}
-      </h1>
-      <h2 key={repo.description}>{repo.description}</h2>
-      <button type="button">
-        <a href={repo.html_url} target="_blank" key={repo.html_url}>
-          GitHub Repo
-        </a>
-      </button>
+    <div
+      className={`repo__card ${addClass(repo.language)}`}
+      key={repo.fullName}
+    >
+      <div>
+        <h1 key={repo.name}>
+          <img src={folder} alt="folder" width="38px" height="36px" />
+          {repo.name}
+        </h1>
+        <h2 key={repo.description}>{repo.description}</h2>
+        <h3 key={repo.language}>{repo.language}</h3>
+      </div>
+      <div className="repo__card--buttons">
+        <button type="button">
+          <a
+            href={repo.homepage}
+            target="_blank"
+            key={repo.homepage}
+            rel="noreferrer"
+          >
+            Live Demo
+          </a>
+        </button>
+        <button type="button">
+          <a
+            href={repo.html_url}
+            target="_blank"
+            key={repo.html_url}
+            rel="noreferrer"
+          >
+            GitHub Repo
+          </a>
+        </button>
+      </div>
     </div>
   ));
   return <div className="repo">{listedRepos}</div>;
